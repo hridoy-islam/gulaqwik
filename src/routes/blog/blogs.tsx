58cdf9f8d7403f8f396dfeee0d67884feb20a487
mushfik16381 <ahmed.mushfik16381@gmail.com>
2023-05-18 00:42:34 +0600

    blog dynamic

--- src/routes/blog/blogs.tsx
+++ src/routes/blog/blogs.tsx
@@ -1,54 +1,21 @@
 import { component$ } from "@builder.io/qwik";
 import styles from './blog.module.css';
 
-export default component$(() => {
+export default component$(({blog}) => {
+    console.log(blog)
     return (
         <>
-            <div class={styles.blog_scroll}>
-                <div class="grid grid-cols-3">
-                    <div>
+        <div>
                         <a href="#" class={styles.preview}>
-                            <img src="https://images.pexels.com/photos/1379636/pexels-photo-1379636.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+                            <img src={blog?.previewImage} class={styles.postPreviewImage} alt="img" />
                             <div>
                                 <p class={styles.previewDate}>8 de marzo de 2023</p>
                             </div>
-                            <h2 class={styles.title}>Sexo Anal, una práctica segura</h2>
-                            <h3 class={styles.description}>Un sex shop puede ser una solución para aquellos que buscan mejorar su pareja o también poder mimarse a sí mismos y experimentar nuevas sensaciones.</h3>
+                            <h2 class={styles.title}>{blog?.title}</h2>
+                            <h3 class={styles.description}>{blog?.seoDescription}</h3>
                         </a>
                     </div>
-                    <div>
-                        <a href="#" class={styles.preview}>
-                            <img src="https://images.pexels.com/photos/1379636/pexels-photo-1379636.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
-                            <div>
-                                <p class={styles.previewDate}>8 de marzo de 2023</p>
-                            </div>
-                            <h2 class={styles.title}>Sexo Anal, una práctica segura</h2>
-                            <h3 class={styles.description}>Un sex shop puede ser una solución para aquellos que buscan mejorar su pareja o también poder mimarse a sí mismos y experimentar nuevas sensaciones.</h3>
-                        </a>
-                    </div>
-                    <div>
-                        <a href="#" class={styles.preview}>
-                            <img src="https://images.pexels.com/photos/1379636/pexels-photo-1379636.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
-                            <div>
-                                <p class={styles.previewDate}>8 de marzo de 2023</p>
-                            </div>
-                            <h2 class={styles.title}>Sexo Anal, una práctica segura</h2>
-                            <h3 class={styles.description}>Un sex shop puede ser una solución para aquellos que buscan mejorar su pareja o también poder mimarse a sí mismos y experimentar nuevas sensaciones.</h3>
-                        </a>
-                    </div>
-                    <div>
-                        <a href="#" class={styles.preview}>
-                            <img src="https://images.pexels.com/photos/1379636/pexels-photo-1379636.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
-                            <div>
-                                <p class={styles.previewDate}>8 de marzo de 2023</p>
-                            </div>
-                            <h2 class={styles.title}>Sexo Anal, una práctica segura</h2>
-                            <h3 class={styles.description}>Un sex shop puede ser una solución para aquellos que buscan mejorar su pareja o también poder mimarse a sí mismos y experimentar nuevas sensaciones.</h3>
-                        </a>
-                    </div>
-                    
-                </div>
-            </div>
+            
         </>
     )
 })
