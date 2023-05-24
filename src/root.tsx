bf13e33c0a8626f2ec62424a7a92de09633b4d6b
Marcell Calero <marcell93c@gmail.com>
2023-05-24 12:10:28 -0300

    ad GA

--- src/root.tsx
+++ src/root.tsx
@@ -1,5 +1,6 @@
 import { component$, createContextId, useContextProvider, useStore } from '@builder.io/qwik';
 import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
+import { QwikPartytown } from './components/partytown/partytown';
 import { RouterHead } from './components/router-head/router-head';
 
 import './global.css';
@@ -27,9 +28,23 @@ export default component$(() => {
       <head>
         <meta charSet="utf-8" />
         <link rel="manifest" href="/manifest.json" />
+        <meta property="og:title" content="Gula" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://gula.com.uy" />
+        <meta property="og:image" content="/assets/logos/gula-perfil.jpg" />
+        <meta property="og:description"
+          content="Somos la primera Red Social Erótica de Chicas Escorts. Gula un espacio de publicidad donde putas, mujeres trans y hombres, ofrecen sus diferentes servicios." />
+        <meta property="og:site_name" content="https://gula.com.uy" />
+        <meta name="rating" content="adult"></meta>
+        <QwikPartytown forward={['dataLayer.push']} />
+        <script
+          async
+          type="text/partytown"
+          src="https://www.googletagmanager.com/gtag/js?id=UA-103777622-2"
+        />
         <RouterHead />
       </head>
-      <body lang="en">
+      <body lang="es">
         <RouterOutlet />
         <ServiceWorkerRegister />
       </body>
